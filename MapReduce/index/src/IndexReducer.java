d84e1ff796ddce7baa6bb243082e28fc8a59d2d0
ElodieC <elodie.corbel@insa-rennes.fr>
2013-03-06 16:39:27 +0100

    Writing IndexReducer

--- MapReduce/index/src/IndexReducer.java
+++ MapReduce/index/src/IndexReducer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -7,16 +8,22 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class IndexReducer extends Reducer<Text,LongWritable,Text,Text> {
 
-	public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException,InterruptedException {
-		// replace KeyType with the real type of your key
-		KeyType key = (KeyType) _key;
-
+	public void reduce(Text key, Iterator<LongWritable> values, Context context) throws IOException,InterruptedException {
+		
+		//pour stocker les offsets et les ecrire a la fin
+		String offsets="";
+		final String separator = ",";
+		
 		while (values.hasNext()) {
-			// replace ValueType with the real type of your value
-			ValueType value = (ValueType) values.next();
-
-			// process value
+			LongWritable value = (LongWritable) values.next();
+			if (offsets.equals("")){//oui c'est pas joli je sais pas comment faire autrement
+				offsets=value.toString();
+			}
+			else{
+				offsets+=separator+value.toString();
+			}
 		}
+		context.write(key,new Text(offsets));
 	}
 
 }
