2ec7a4c7167ca2fa5e0ca0ecd20ad12b7d4c4d5a
ElodieC <elodie.corbel@insa-rennes.fr>
2013-04-21 17:53:47 +0200

    utilisation string builder

--- MapReduce/index/src/IndexReducer.java
+++ MapReduce/index/src/IndexReducer.java
@@ -9,18 +9,18 @@ public class IndexReducer extends Reducer<Text,Text,Text,Text> {
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException,InterruptedException {
 		
 		//pour stocker les offsets et les ecrire a la fin
-		String offsets="";
+		StringBuilder offsets = new StringBuilder();
 		final String separator = " , ";
 		
 		for (Text value:values) {
-			if (offsets.equals("")){//oui c'est pas joli je sais pas comment faire autrement
-				offsets=value.toString();
+			if (offsets.toString().equals("")){//oui c'est pas joli je sais pas comment faire autrement
+				offsets.append(value.toString());
 			}
 			else {
-				offsets+=separator+value.toString();
+				offsets.append(separator+value.toString());
 			}
 		}
-		context.write(key,new Text(offsets));
+		context.write(key,new Text(offsets.toString()));
 	}
 
 }
